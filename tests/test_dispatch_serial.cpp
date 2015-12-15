caef04d05c7c799915e8eb1ffcbe76339e62a9f4
Zizheng Tai <zizheng.tai@gmail.com>
2015-12-14 22:37:54 -0800

    added tests

---
+++ tests/test_dispatch_serial.cpp
@@ -0,0 +1,42 @@
+#include <iterator>
+#include <mutex>
+#include <vector>
+
+#include "gungnir/gungnir.hpp"
+
+#include "catch.hpp"
+
+SCENARIO("serial dispatch maintains order of tasks", "[serial]") {
+
+    std::vector<int> v;
+    std::mutex m;
+
+    GIVEN("an ordered sequence of tasks") {
+
+        std::vector<gungnir::Task> tasks;
+        for (int i = 0; i < 1000; ++i) {
+            tasks.emplace_back([i, &v, &m] {
+                std::unique_lock<std::mutex> lk(m);
+                v.emplace_back(i);
+            });
+        }
+
+        WHEN("serial-dispatched") {
+
+            gungnir::TaskPool{8}.dispatchSerial(
+                    std::cbegin(tasks), std::cend(tasks));
+
+            THEN("they are executed in the identical order") {
+
+                bool matched = true;
+                for (int i = 0; i < 1000; ++i) {
+                    if (v[i] != i) {
+                        matched = false;
+                        break;
+                    }
+                }
+                REQUIRE(matched);
+            }
+        }
+    }
+}
