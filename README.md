1e339b9fae5cdd19a6fb5fa9bd5254520ce94d3b
Zizheng Tai <zizheng.tai@gmail.com>
2015-12-20 00:00:30 -0800

    using consumer tokens & added copyright info

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 [![Build Status](https://travis-ci.org/ZizhengTai/gungnir.svg?branch=master)](https://travis-ci.org/ZizhengTai/gungnir)
 [![License](https://img.shields.io/badge/license-Apache_2.0-blue.svg)](LICENSE)
 
+Gungnir is aa modern and easy-to-use C++ library for concurrent task scheduling.
+
+## License
+
+This project is licensed under the Apache License 2.0. See the [LICENSE](./LICENSE) file for details.
--- include/gungnir/gungnir.hpp
+++ include/gungnir/gungnir.hpp
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Zizheng Tai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #ifndef GUNGNIR_HPP
 #define GUNGNIR_HPP
 
@@ -30,12 +46,13 @@ public:
 
         for (std::size_t i = 0; i < numThreads_; ++i) {
             threads_.emplace_back([this] {
+                moodycamel::ConsumerToken ctok{tasks_};
                 Task t;
 
-                tasks_.wait_dequeue(t);
+                tasks_.wait_dequeue(ctok, t);
                 while (t) {
                     t();
-                    tasks_.wait_dequeue(t);
+                    tasks_.wait_dequeue(ctok, t);
                 }
             });
         }
@@ -56,10 +73,11 @@ public:
         std::atomic_size_t numDoneThreads{0};
         for (auto &t: threads_) {
             t = std::thread([this, &numDoneThreads] {
+                moodycamel::ConsumerToken ctok{tasks_};
                 Task t;
 
                 do {
-                    while (tasks_.try_dequeue(t)) {
+                    while (tasks_.try_dequeue(ctok, t)) {
                         t();
                     }
                 } while (numDoneThreads.fetch_add(
